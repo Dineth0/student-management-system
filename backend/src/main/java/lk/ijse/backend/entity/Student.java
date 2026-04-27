bdbfaad1e9f9627a2cf87c4169d07a00f719037e
Dineth0 <dinethnakandala10@gmail.com>
2026-04-27 08:45:47 +0530

    feat(database): add student entity

---
+++ backend/src/main/java/lk/ijse/backend/entity/Student.java
@@ -0,0 +1,26 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private Date birthday;
+    private String nic;
+}

11da4621063d86e130fc88d22f5e9b62b6e07c1a
Dineth0 <dinethnakandala10@gmail.com>
2026-04-27 10:50:44 +0530

    feat(controller):implement student update endpoint

---
+++ backend/src/main/java/lk/ijse/backend/controller/StudentController.java
@@ -0,0 +1,45 @@
+package lk.ijse.backend.controller;
+
+import lk.ijse.backend.dto.ResponseDTO;
+import lk.ijse.backend.dto.UserRegistrationDTO;
+import lk.ijse.backend.service.StudentService;
+import lk.ijse.backend.util.VarList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/student")
+@CrossOrigin
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @PutMapping("/updateStudent")
+    public ResponseEntity<ResponseDTO> updateStudent(@RequestBody UserRegistrationDTO userRegistrationDTO) {
+        try{
+            int response = studentService.UpdateStudent(userRegistrationDTO);
+
+            switch (response) {
+                case VarList.Updated -> {
+                    return ResponseEntity.status(HttpStatus.CREATED)
+                            .body(new ResponseDTO(VarList.Created, "Student Saved", userRegistrationDTO));
+                }
+                case VarList.Not_Found -> {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                            .body(new ResponseDTO(VarList.Not_Found, "Student Not found", null));
+                }
+                default -> {
+                    return ResponseEntity.status(HttpStatus.CREATED)
+                            .body(new ResponseDTO(VarList.Internal_Server_Error, "Error while updating", null));
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO(VarList.Internal_Server_Error, e.getMessage(), null));
+        }
+    }
+}

7f4fe0ef8a060fafc3f7d2b90516b7b03857837d
Dineth0 <dinethnakandala10@gmail.com>
2026-04-29 14:48:25 +0530

    feat(ui):add course page nav bar for layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { AuthProvider } from "@/context/AuthContext";
+import CoursePageNavbar from "@/components/coursePageNavbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <AuthProvider>
           <Navbar/>
+          <CoursePageNavbar/>
           {children}
         </AuthProvider>
         

60e7af230f6e3cf203c6dca158b239ccbcac9b99
Dineth0 <dinethnakandala10@gmail.com>
2026-04-29 14:22:51 +0530

    feat(ui):add logout

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,14 +1,27 @@
 'use client'
 
+import { useAuth } from '@/context/AuthContext'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 export default function Navbar() {
   const pathname = usePathname()
   const path = ["/", "/signup", '/coursesPage']
+  const {logout} = useAuth()
+  const router = useRouter()
+  const {setUser} = useAuth()
 
   if(path.includes(pathname)){
     return null
+  }
+
+  const handleLogout = async()=>{
+    try{
+      await logout()
+      router.push('/')
+    }catch(error){
+      console.error("Logout field", error)
+    }
   }
   return (
     <nav className='flex justify-between p-4 bg-green-700 text-white shadow-lg'>
@@ -17,7 +30,7 @@ export default function Navbar() {
             <Link href="/dashboard" className='hover:underline'>Dashboard</Link>
             <Link href="/students" className='hover:underline'>Students</Link>
             <Link href="/course" className='hover:underline'>Courses</Link>
-            <button className='bg-red-500 px-3 py-1 rounded'>Logout</button>
+            <button className='bg-red-500 px-3 py-1 rounded' onClick={handleLogout}>Logout</button>
         </div>
     </nav>
   )

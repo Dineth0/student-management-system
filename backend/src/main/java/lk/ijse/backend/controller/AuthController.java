44b8c82195ce291fc7bf22b0e7aa9fc47c5c2f4f
Dineth0 <dinethnakandala10@gmail.com>
2026-04-29 12:27:33 +0530

    feat(controller):add auth controller for auth endpoints

---
+++ backend/src/main/java/lk/ijse/backend/controller/AuthController.java
@@ -0,0 +1,106 @@
+package lk.ijse.backend.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.backend.dto.AuthDTO;
+import lk.ijse.backend.dto.ResponseDTO;
+import lk.ijse.backend.dto.UserRegistrationDTO;
+import lk.ijse.backend.service.UserService;
+import lk.ijse.backend.util.JwtUtil;
+import lk.ijse.backend.util.VarList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> login(@RequestBody UserRegistrationDTO userRegistrationDTO, HttpServletResponse response) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userRegistrationDTO.getEmail(), userRegistrationDTO.getPassword()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ResponseDTO(VarList.Unauthorized, "Invalid Credentials", e.getMessage()));
+        }
+
+        UserRegistrationDTO loadedUser = userService.loadUserDetailsByUsername(userRegistrationDTO.getEmail());
+        if (loadedUser == null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new ResponseDTO(VarList.Conflict, "Authorization Failure! Please Try Again", null));
+        }
+
+        String token = jwtUtil.generateToken(loadedUser);
+        if (token == null || token.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new ResponseDTO(VarList.Conflict, "Authorization Failure! Please Try Again", null));
+        }
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 10);
+
+        response.addCookie(cookie);
+
+        AuthDTO authDTO = new AuthDTO();
+        authDTO.setEmail(loadedUser.getEmail());
+        authDTO.setRole(loadedUser.getRole().toString());
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseDTO(VarList.OK, "Success", authDTO));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ResponseDTO> getCurrentUser() {
+        try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String email = authentication.getName();
+            UserRegistrationDTO userRegistrationDTO = userService.loadUserDetailsByUsername(email);
+
+            if (userRegistrationDTO != null) {
+                userRegistrationDTO.setPassword(null);
+
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(new ResponseDTO(VarList.OK, "Success", userRegistrationDTO));
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ResponseDTO(VarList.Not_Found, "User Not found", null));
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO(VarList.Internal_Server_Error, e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ResponseDTO>logout(HttpServletResponse response){
+        Cookie cookie = new Cookie("token", null);
+
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseDTO(VarList.OK, "Successfully logout", null));
+    }
+}

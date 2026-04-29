0317025b64fe902d3a7faf02edbb7a08957e2ef4
Dineth0 <dinethnakandala10@gmail.com>
2026-04-29 11:47:10 +0530

    feat(service):add load user details logic for user

--- backend/src/main/java/lk/ijse/backend/service/UserService.java
+++ backend/src/main/java/lk/ijse/backend/service/UserService.java
@@ -3,10 +3,14 @@ package lk.ijse.backend.service;
 import lk.ijse.backend.dto.ResponseDTO;
 import lk.ijse.backend.dto.UserRegistrationDTO;
 import lk.ijse.backend.entity.Student;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
 public interface UserService {
     public int AddUser(UserRegistrationDTO userRegistrationDTO);
 
+    UserDetails loadUserByUsername(String email);
+
+    UserRegistrationDTO loadUserDetailsByUsername(String name);
 }
--- backend/src/main/java/lk/ijse/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/lk/ijse/backend/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package lk.ijse.backend.service.impl;
 import jakarta.transaction.Transactional;
 import lk.ijse.backend.dto.ResponseDTO;
 import lk.ijse.backend.dto.UserRegistrationDTO;
+import lk.ijse.backend.entity.Role;
 import lk.ijse.backend.entity.Student;
 import lk.ijse.backend.entity.User;
 import lk.ijse.backend.repo.StudentRepo;
@@ -11,11 +12,19 @@ import lk.ijse.backend.service.StudentService;
 import lk.ijse.backend.service.UserService;
 import lk.ijse.backend.util.VarList;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +34,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepo userRepo;
     private final StudentRepo studentRepo;
     private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
 
     @Override
@@ -39,7 +49,8 @@ public class UserServiceImpl implements UserService {
 
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             user.setPassword(passwordEncoder.encode(userRegistrationDTO.getPassword()));
-
+            user.setRole(Role.STUDENT);
+            userRegistrationDTO.setRole(Role.STUDENT);
             userRepo.save(user);
 
             Student student = new Student();
@@ -55,6 +66,36 @@ public class UserServiceImpl implements UserService {
         }catch(Exception e){
             return VarList.Internal_Server_Error;
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepo.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                getAuthority(user)
+        );
+    }
+
+    @Override
+    public UserRegistrationDTO loadUserDetailsByUsername(String name) {
+        User user =  userRepo.findByEmail(name);
+        if(user == null){
+            throw new UsernameNotFoundException("user not found");
+        }
+        UserRegistrationDTO userRegistrationDTO = modelMapper.map(user, UserRegistrationDTO.class);
+        userRegistrationDTO.setRole(user.getRole());
+        return userRegistrationDTO;
+    }
+
+    private Set<SimpleGrantedAuthority> getAuthority(User user) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+        return authorities;
     }
 
 }

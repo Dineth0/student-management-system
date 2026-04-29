1367e97857aeb04e56b8969e2aef4138c3ac2fcb
Dineth0 <dinethnakandala10@gmail.com>
2026-04-29 14:23:59 +0530

    feat(ui):add middleware

---
+++ frontend/middleware/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export default function middleware(request:NextRequest) {
+    const token = request.cookies.get("token")?.value
+    const pathname = request.nextUrl.pathname;
+
+    const protectedRoutes = ["/dashboard", "/students","/course","/coursesPage"]
+
+    if(protectedRoutes.includes(pathname) && !token){
+        return NextResponse.redirect(new URL("/", request.url))
+    }
+    return NextResponse.next()
+}
+export const config = {
+    matcher:[
+        "/dashboard/:path*",
+        "/students/:path*",
+        "/course/:path*"
+    ]
+}

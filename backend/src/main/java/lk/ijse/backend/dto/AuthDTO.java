c6ccf5884e1571ff0acb9fd7ba30908a6e1b7277
Dineth0 <dinethnakandala10@gmail.com>
2026-04-29 09:28:27 +0530

    feat(dto):add auth dto

---
+++ backend/src/main/java/lk/ijse/backend/dto/AuthDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AuthDTO {
+    private String token;
+    private String email;
+    private String role;
+}
